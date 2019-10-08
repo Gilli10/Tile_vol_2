3e6f5f4c5f2c6d31d9bf5619f6135f5577252caa
Gisli Thor <gislig18@ru.is>
2019-10-08 09:56:50 +0000

    N;stum bui[

--- Tile_traveler_vol2.py
+++ Tile_traveler_vol2.py
@@ -1,127 +1,128 @@
 coins = 0
 
-def x1y1(coins):
-    print("You can travel: (N)orth.")
+def x1y1():
     status = True
     while status == True:
+        print("You can travel: (N)orth.")
         input_dir = inputdir()
         if input_dir == "n" or input_dir == "N":
             status = False
-            x1y2(coins)
+            x1y2()
         else:
             print("Not a valid direction!")
 
 
-def x1y2(coins):
-    pull_lever(coins)
+def x1y2():
+    pull_lever()
     print("You can travel: (N)orth or (E)ast or (S)outh.")
     status = True
     while status == True:
         input_dir = inputdir()
         if input_dir == "n" or input_dir == "N":
             status = False
-            x1y3(coins)
+            x1y3()
         elif input_dir == "e" or input_dir == "E":
             status = False
-            x2y2(coins)
+            x2y2()
         elif input_dir == "s" or input_dir == "S":
             status = False
-            x1y1(coins)
+            x1y1()
         else:
             print("Not a valid direction!")
 
 
-def x1y3(coins):
+def x1y3():
     print("You can travel: (E)ast or (S)outh.")
     status = True
     while status == True:
         input_dir = inputdir()
         if input_dir == "e" or input_dir == "E":
             status = False
-            x2y3(coins)
+            x2y3()
         elif input_dir == "s" or input_dir == "S":
             status = False
-            x1y2(coins)
+            x1y2()
         else:
             print("Not a valid direction!")
 
 
-def x2y1(coins):
+def x2y1():
     print("You can travel: (N)orth.")
     status = True
     while status == True:
         input_dir = inputdir()
         if input_dir == "n" or input_dir == "N":
             status = False
-            x2y2(coins)
+            x2y2()
         else:
             print("Not a valid direction!")
 
 
-def x2y2(coins):
-    pull_lever(coins)
+def x2y2():
+    pull_lever()
     print("You can travel: (S)outh or (W)est.")
     status = True
     while status == True:
         input_dir = inputdir()
         if input_dir == "w" or input_dir == "W":
             status = False
-            x1y2(coins)
+            x1y2()
         elif input_dir == "s" or input_dir == "S":
             status = False
-            x2y1(coins)
+            x2y1()
         else:
             print("Not a valid direction!")
 
 
-def x2y3(coins):
-    pull_lever(coins)
+def x2y3():
+    pull_lever()
     print("You can travel: (E)ast or (W)est.")
     status = True
     while status == True:
         input_dir = inputdir()
         if input_dir == "e" or input_dir == "E":
             status = False
-            x3y3(coins)
+            x3y3()
         elif input_dir == "w" or input_dir == "W":
             status = False
-            x1y3(coins)
+            x1y3()
         else:
             print("Not a valid direction!")
 
 
 # If this function is called, "Victory!" gets printed out.
-def x3y1(coins):
-    print("Victory! Total coins",coins)
+def x3y1():
+    global coins
+    print("Victory! Total coins {}.".format(coins))
 
 
-def x3y2(coins):
-    pull_lever(coins)
+def x3y2():
+    pull_lever()
     print("You can travel: (N)orth or (S)outh.")
     status = True
     while status == True:
         input_dir = inputdir()
         if input_dir == "n" or input_dir == "N":
             status = False
-            x3y3(coins)
+            x3y3()
         elif input_dir == "s" or input_dir == "S":
             status = False
-            x3y1(coins)
+            x3y1()
         else:
             print("Not a valid direction!")
 
 
-def x3y3(coins):
+def x3y3():
     print("You can travel: (S)outh or (W)est.")
     status = True
     while status == True:
         input_dir = inputdir()
         if input_dir == "w" or input_dir == "W":
             status = False
-            x2y3(coins)
+            x2y3()
         elif input_dir == "s" or input_dir == "S":
             status = False
-            x3y2(coins)
+            x3y2()
         else:
             print("Not a valid direction!")
 
@@ -131,13 +132,14 @@ def inputdir():
     direction = input("Direction: ")
     return direction
 
-def pull_lever(coins):
+def pull_lever():
     anwser = input("Pull a lever (y/n): ")
     if anwser == "y" or anwser == "Y":
-        coins +=1
+        global coins
+        coins = coins +1
         print("You received 1 coin, your total is now {}.".format(coins))
 
 
 
 # Starts the game with the player in box [1, 1]
-x1y1(coins)
+x1y1()
